e9dd17e7d934f93fa0fffa18c095dc1c33605b43
Jan Benda <jan.benda@uni-tuebingen.de>
2024-04-23 22:21:20 +0200

    added modules

---
+++ src/thunderlab/version.py
@@ -0,0 +1,9 @@
+__version__='1.0.0'
+"""Current version of the thunderlab package."""
+
+__year__ = '2024'
+"""Current year for copyright messages."""
+
+__pdoc__ = {}
+__pdoc__['__version__'] = True
+__pdoc__['__year__'] = True
